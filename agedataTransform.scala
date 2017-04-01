9b0e4d1c97d358ea38ae3ff93588cbe80bab870d
lvyanjie_sx <lvyanjie_sx@qiyi.com>
2017-04-01 14:18:16 +0800

    预测结果集正确率为74%

--- agedataTransform.scala
+++ agedataTransform.scala
@@ -5,7 +5,7 @@ import org.apache.spark.mllib.tree._
 import org.apache.spark.mllib.tree.model._
 import org.apache.spark.rdd._
 
-val rawData = sc.textFile("file:///root/kaggle/kaggletitanic/test.csv").filter(!_.contains("PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked"))
+val rawData = sc.textFile("file:///root/Downloads/train.csv").filter(!_.contains("PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked"))
 
 var data = rawData.map(line => {
   // line = 1,0,3,"Braund, Mr. Owen Harris",male,22,1,0,A/5 21171,7.25,,S
@@ -19,46 +19,24 @@ var data = rawData.map(line => {
   }
   val str = line.substring(0,start-1)+line.substring(i+1)
   str.split(",").tail
+}).filter(_.size == 10).map(r => {
+  var tmp = r
+  tmp(2) = sexMap(tmp(2))
+  tmp(9) = embarkedMap(tmp(9))
+  tmp
 })
 
-data = data.filter(_.size > 9).map(arr => {
-  arr.slice(0,6):+arr(7):+arr(9)
-})
-
-val sexMap = Map(
-  "male" -> "0",
-  "female" -> "1"
-)
-
-val embarkedMap = Map(
-  "S" -> "0",
-  "Q" -> "1",
-  "C" -> "2"
-)
-// delete the record which the element is empty
-data = data.filter(r => {
-  var flag = true
-  for(str <- r if flag){
-    if(str.equals("")){
-      flag = false
-    }
-  }
-  flag
-})
+data = data.filter(_(3) == "").map(r => r.slice(0,3)++:r.slice(4,6):+r(7):+r(9))
 
 val Array(trainData,cvData,testData) = data.map(r => {
-  var arr = r
-  arr(2) = sexMap(arr(2))
-  arr(7) = embarkedMap(arr(7))
-  val feature = arr.tail.map(_.toDouble)
-  val label = arr.head.toDouble 
-  LabeledPoint(label,Vectors.dense(feature))
+  val featurevector = r.tail.map(_.toDouble)
+  val label = r.head.toDouble
+  LabeledPoint(label,Vectors.dense(featurevector))
 }).randomSplit(Array(0.8,0.1,0.1))
 trainData.cache()
 cvData.cache()
 testData.cache()
 
-val model = DecisionTree.trainClassifier(trainData,2,Map(1 -> 2,6 -> 3),"gini",4,30)
 
 def getMetrics(model:DecisionTreeModel,data:RDD[LabeledPoint]):MulticlassMetrics = {
   val predictAndLabel = data.map(example => {
@@ -69,24 +47,52 @@ def getMetrics(model:DecisionTreeModel,data:RDD[LabeledPoint]):MulticlassMetrics
 val metrics = getMetrics(model,cvData)
 
 val evaluations = for(impurity <- Array("gini","entropy");depth <- Array(4,30);bins <- Array(50,100,300)) yield {
-  val model = DecisionTree.trainClassifier(trainData,2,Map(1 -> 2,6 -> 3),impurity,depth,bins)
+  val model = DecisionTree.trainClassifier(trainData,2,Map(1 -> 2,5 -> 3),impurity,depth,bins)
   val metrics = getMetrics(model,cvData)
   ((impurity,depth,bins),metrics.precision)
 }
+
 
 val (impurity,depth,bins) = evaluations.sortBy(_._2).reverse.head._1
+val model = DecisionTree.trainClassifier(trainData,2,Map(1 -> 2,5 ->3),impurity,depth,bins)
 
-val forest = RandomForest.trainClassifier(trainData,2,Map(1 -> 2,6 ->3),20,"auto",impurity,depth,bins)
-def getMetrics(model:RandomForestModel,data:RDD[LabeledPoint]):MulticlassMetrics = { 
-  val predictAndLabel = data.map(example => {
-    (model.predict(example.features),example.label)
-  })  
-  new MulticlassMetrics(predictAndLabel)
-}
+//对没有age字段的记录进行预测
+var testRawData = sc.textFile("file:///root/Downloads/test.csv")
 
-val evaluations = for(impurity <- Array("gini","entropy");depth <- Array(10,20,30);bins <- Array(50,100,200)) yield {
-  val model = RandomForest.trainClassifier(trainData,2,Map(1 -> 2,6 -> 3),20,"auto",impurity,depth,bins)
-  val metrics = getMetrics(model,cvData)
-  ((impurity,depth,bins),metrics.precision)
-}
+var data = testRawData.map(line => {
+  // line = 1,3,"Braund, Mr. Owen Harris",male,22,1,0,A/5 21171,7.25,,S
+  // delete Name arrtibute
+  val start = line.indexOf("\"")
+  var i = 0
+  for(index <- (start+1 until line.length)){
+    if(line(index) == '"'){
+        i = index
+    }
+  }
+  val str = line.substring(0,start-1)+line.substring(i+1)
+  val id = str.split(",").head
+  var arr = str.split(",").tail
+  arr(1) = sexMap(arr(1))
+  arr(8) = embarkedMap(arr(8))
+  val tmp = arr.slice(0,5):+arr(6):+arr(8)
+  (id -> tmp)
+})
 
+//data = data.filter(r => {
+//  var flag = true
+//  for(str <- r._2 if flag){
+//    if(str.equals("")){
+//      flag = false
+//    }
+//  }
+//  flag
+//})
+
+// only remain the record which has no age
+data = data.filter(r => r._2(2) == "").map(r => {
+  (r._1,r._2.filter(_ != ""))
+})
+
+val withnoageresult = data.map(r => {
+  (r._1 -> model.predict(Vectors.dense(r._2.map(_.toDouble))))
+})
--- datatransform.scala
+++ datatransform.scala
@@ -20,7 +20,7 @@ var data = rawData.map(line => {
   val str = line.substring(0,start-1)+line.substring(i+1)
   str.split(",").tail
   
-})
+}).filter(_.size == 10)
 
 data = data.filter(_.size > 9).map(arr => {
   arr.slice(0,6):+arr(7):+arr(9)
@@ -59,25 +59,6 @@ trainData.cache()
 cvData.cache()
 testData.cache()
 
-val model = DecisionTree.trainClassifier(trainData,2,Map(1 -> 2,6 -> 3),"gini",4,30)
-
-def getMetrics(model:DecisionTreeModel,data:RDD[LabeledPoint]):MulticlassMetrics = {
-  val predictAndLabel = data.map(example => {
-    (model.predict(example.features),example.label)
-  })
-  new MulticlassMetrics(predictAndLabel)
-}
-val metrics = getMetrics(model,cvData)
-
-val evaluations = for(impurity <- Array("gini","entropy");depth <- Array(4,30);bins <- Array(50,100,300)) yield {
-  val model = DecisionTree.trainClassifier(trainData,2,Map(1 -> 2,6 -> 3),impurity,depth,bins)
-  val metrics = getMetrics(model,cvData)
-  ((impurity,depth,bins),metrics.precision)
-}
-
-val (impurity,depth,bins) = evaluations.sortBy(_._2).reverse.head._1
-
-val forest = RandomForest.trainClassifier(trainData,2,Map(1 -> 2,6 ->3),20,"auto",impurity,depth,bins)
 def getMetrics(model:RandomForestModel,data:RDD[LabeledPoint]):MulticlassMetrics = { 
   val predictAndLabel = data.map(example => {
     (model.predict(example.features),example.label)
@@ -90,6 +71,9 @@ val evaluations = for(impurity <- Array("gini","entropy");depth <- Array(10,20,3
   val metrics = getMetrics(model,cvData)
   ((impurity,depth,bins),metrics.precision)
 }
+
+val (impurity,depth,bins) = evaluations.sortBy(_._2).reverse.head._1
+val forest = RandomForest.trainClassifier(trainData,2,Map(1 -> 2,6 ->3),20,"auto",impurity,depth,bins)
 
 //对含有age字段的记录进行预测
 var testRawData = sc.textFile("file:///root/Downloads/test.csv")
@@ -129,20 +113,3 @@ data = data.filter(r => r._2(2) != "")
 val withageresult = data.map(r => {
   (r._1 -> forest.predict(Vectors.dense(r._2.map(_.toDouble))))
 })
-
-var data = rawData.map(line => {
-  // line = 1,0,3,"Braund, Mr. Owen Harris",male,22,1,0,A/5 21171,7.25,,S
-  // delete Name arrtibute
-  val start = line.indexOf("\"")
-  var i = 0
-  for(index <- (start+1 until line.length)){
-    if(line(index) == '"'){
-        i = index
-    }
-  }
-  val str = line.substring(0,start-1)+line.substring(i+1)
-  str.split(",").tail
-
-})
-
-data = data.filter(_(3) == "")
